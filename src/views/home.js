13527fbddf22c1ed71d4ff2b629316ba930e8bc7
Rasmus <rasmuspriv@gmail.com>
2020-04-08 17:40:59 +0200

    Add min height

--- src/views/home.js
+++ src/views/home.js
@@ -36,13 +36,15 @@ module.exports = `
     }
     textarea {
       width: 90vw;
-      max-width: 90vw;
       height: 80vh;
+      max-width: 90vw;
+      max-height: 80vh;
       color: white;
       background-color: #232327;
       border: 1px solid #444;
       padding: 12px;
       min-width: 200px;
+      min-height: 200px;
     }
   </style>
 </head>
